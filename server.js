4f578e2f7d65d148bf5958a80a7c28a54d4e1799
LighthawkKnight <lighthawkknight@gmail.com>
2018-11-17 00:42:25 -0800

    fixed server.js bugs

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routers
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // Listener
 app.listen(PORT, () => {
